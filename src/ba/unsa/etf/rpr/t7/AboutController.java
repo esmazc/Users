efe742340bf5a3bf2d8d8d73d8833e5829f2fd6b
Esma <ezejnilovi1@etf.unsa.ba>
2022-01-07 22:26:42 +0100

    Implementirana metoda initialize koja postavlja text na TextArea oAutoru

--- src/ba/unsa/etf/rpr/t7/AboutController.java
+++ src/ba/unsa/etf/rpr/t7/AboutController.java
@@ -1,16 +1,26 @@
 package ba.unsa.etf.rpr.t7;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import javafx.scene.control.TextArea;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ResourceBundle;
 
 public class AboutController {
     public Button btnCancel;
+    public TextArea oAutoru;
+
+    @FXML
+    public void initialize() {
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        oAutoru.setText(bundle.getString("text"));
+    }
 
     public void cancelAction(ActionEvent actionEvent) {
         ((Stage)btnCancel.getScene().getWindow()).close();
