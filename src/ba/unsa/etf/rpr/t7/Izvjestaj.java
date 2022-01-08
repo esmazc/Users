6b5dcccb034cc5eedc1ea595d1ee401ddd70647e
Esma <ezejnilovi1@etf.unsa.ba>
2022-01-08 13:09:45 +0100

    implentirana metoda showReport

---
+++ src/ba/unsa/etf/rpr/t7/Izvjestaj.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.t7;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Izvjestaj extends JFrame {
+    public void showReport(Connection conn) throws JRException {
+        String reportSrcFile = getClass().getResource("/reports/korisnici.jrxml").getFile();
+        String reportsDir = getClass().getResource("/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JasperViewer viewer = new JasperViewer(print, false);
+        viewer.setVisible(true);
+    }
+}
