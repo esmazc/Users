cdbfed52f9f827c22f33f376d7bf9645cd3611af
Esma <ezejnilovi1@etf.unsa.ba>
2022-01-11 19:32:52 +0100

    Dodani atributi tilePane, scrollPane, korisnikController i setter setKorisnikController

--- src/ba/unsa/etf/rpr/t7/PretragaSlikeController.java
+++ src/ba/unsa/etf/rpr/t7/PretragaSlikeController.java
@@ -1,32 +1,33 @@
 package ba.unsa.etf.rpr.t7;
 
+import at.mukprojects.giphy4j.Giphy;
+import at.mukprojects.giphy4j.entity.search.SearchFeed;
+import at.mukprojects.giphy4j.exception.GiphyException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class PretragaSlikeController {
+    public TilePane tilePane;
+    public ScrollPane scrollPane;
     public TextField fldPretraga;
     private String slikaUrl = null;
+    private KorisnikController korisnikController;
 
     public void searchAction(ActionEvent actionEvent) {
-        /*new Thread(() -> {
-            try {
-                URL url = new URL(adresa);
-                BufferedReader ulaz = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-                String json = "", line = null;
-                while ((line = ulaz.readLine()) != null)
-                    json = json + line;
-                if (...) {
-                    Platform.runLater(() -> { polje.setStyle("nije-ispravno"); });
-                } catch( … )
-            }).start();*/
     }
 
     public void okAction(ActionEvent actionEvent) {
@@ -46,5 +47,9 @@ public class PretragaSlikeController {
 
     public void cancelAction(ActionEvent actionEvent) {
         ((Stage)fldPretraga.getScene().getWindow()).close();
+    }
+
+    public void setKorisnikController(KorisnikController korisnikController) {
+        this.korisnikController = korisnikController;
     }
 }
